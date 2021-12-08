dd05a28a93a46ff43bebea8adb70e23f717ced61
Matthew <phillips.matthew829@gmail.com>
2021-12-08 14:25:12 -0600

    commit tree a0a9fb294515521c8ee42b5040105d12d89b68b6

--- index.js
+++ index.js
@@ -1,50 +1,41 @@
-// Add your Circle class here
 /*
-Define getter methods for diameter, circumference, and area (calculate each value using this.radius and pi)
+Define getter methods for diameter, circumference, and area (calculate each value using this.radius and pi).
 
 Define setter methods for diameter, circumference, area which will accept values for each calculation, 
-    calculate the radius based on the input value and set this.radius accordingly
+    calculate radius based on input value and set this.radius accordingly.
 
-All instances should be able to set this.radius by setting a value to diameter, circumference, or area*/
-/*
-//Formulas to convert diameter, circumference, and area (in order) to radius:
-radius = diameter / 2; 
-radius = circumference / 2 / Math.PI
-radius = area / radius / Math.PI
+All instances should be able to set this.radius by setting value to diameter, circumference, or area.
 */
-
 class Circle{
-    //Private properties?
-
     constructor(radius){
         this.radius = radius;
     }
     //Methods
-    //Setter methods:
+    static pi(){
+        return Math.PI;
+    }
+
     set diameter(diameter){ //Set diameter's value.
-        this.diameter = diameter;
+        this.radius = diameter / 2;
     }
 
     set circumference(circumference){ //Set circumference's value.
-        this.circumference = circumference;
+        this.radius = circumference / 2 / Circle.pi();
     }
 
     set area(area){ //Set area's value.
-        this.area = area;
+        this.radius = Math.sqrt(area / Circle.pi());
     }
 
-    //Getter methods:
     get diameter(){
-        return this.diameter = this.radius * 2;
+        return this.radius * 2;
     }
 
     get circumference(){
-        return this.circumference = Math.PI * (this.radius * 2);
+        return Math.PI * (this.radius * 2);
     }
 
     get area(){
-        return this.area = Circle.pi * Math.pow(this.radius, 2);
+        return Circle.pi() * Math.pow(this.radius, 2);
     }
-
-    //
 }
